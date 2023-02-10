73362e697ae5ce918143d1df39d2f185802dc8e5
Enzo Gabriel <enzogabrielcarvalho123@gmail.com>
2023-02-10 13:43:55 -0300

    Criando e stylizando o header

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Filme from './pages/Filme';
+import Header from './components/Header';
 
 function RoutesApp(){
     return(
         <BrowserRouter>
+            <Header/>
             <Routes>
                 <Route path="/" element={ <Home/> } />
                 <Route path="/filme/:id" element={ <Filme/> } />
