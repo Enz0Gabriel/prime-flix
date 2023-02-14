9d83fd725b013fa0df72f4b35dff31a809ad748f
Enzo Gabriel <enzogabrielcarvalho123@gmail.com>
2023-02-14 15:30:48 -0300

    Salvando filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -37,6 +37,25 @@ function Filme(){
         }
     }, [navigate, id])
 
+
+    function salvarFilme(){
+        const minhaLista = localStorage.getItem("@primeflix");
+
+        let filmesSalvos = JSON.parse(minhaLista) || [];
+
+        const hasFilme = filmesSalvos.some( (filmesSalvo) => filmesSalvo.id === filme.id)
+
+        if(hasFilme){
+            alert("ESSE FILME JÁ ESTA NA LISTA");
+            return;
+        }
+
+        filmesSalvos.push(filme);
+        localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+        alert("FILME SALVO COM SUCESSO")
+
+    }
+
     if(loading){
         return(
             <div className="filme-info">
@@ -56,9 +75,9 @@ function Filme(){
             <strong>Avaliação: {filme.vote_average.toFixed(1)} / 10</strong>
 
         <div className="area-buttons">
-            <button>Salvar</button>
+            <button onClick={salvarFilme}>Salvar</button>
             <button>
-                <a target="_blank" rel="external" href={`https://www.youtube.com/results?search_query=${filme.title} Trailer`}>
+                <a target="blank" rel="external" href={`https://www.youtube.com/results?search_query=${filme.title} Trailer`}>
                     Trailer
                 </a>
             </button>
