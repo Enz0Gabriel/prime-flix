0517db4824dcc1d485f223124bf3f5f87b77b89f
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-06-06 14:54:10 -0300

    Creating home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,31 @@
+import { useEffect, useState } from 'react'
+import api from '../../services/api';
+
+//URL DA API: /movie/now_playing?api_key=6172ce52b78bd185b74c4620691772d7&language=pt-BR
 
 function Home (){
+    const [filmes, setFilmes] = useState([]);
+
+
+    useEffect(()=>{
+
+        async function loadFilmes(){
+            const response = await api.get("movie/now_playing", {
+                params:{
+                    api_key:"6172ce52b78bd185b74c4620691772d7",
+                    lenguage: "pt-BR",
+                    page: 1,
+                }
+            })
+
+            console.log(response.data.results);
+
+        }
+
+        loadFilmes();
+
+    }, [])
+
     return(
         <div>
             <h1>BEM VINDO A HOME</h1>
