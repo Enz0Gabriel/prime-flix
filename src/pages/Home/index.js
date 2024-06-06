7bc713a8e364f75a92cac292e2312c9efdaf75c7
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-06-06 15:24:13 -0300

    Film loading

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 import api from '../../services/api';
+import { Link } from 'react-router-dom'
+import './home.css';
 
 //URL DA API: /movie/now_playing?api_key=6172ce52b78bd185b74c4620691772d7&language=pt-BR
 
@@ -13,13 +15,13 @@ function Home (){
             const response = await api.get("movie/now_playing", {
                 params:{
                     api_key:"6172ce52b78bd185b74c4620691772d7",
-                    lenguage: "pt-BR",
+                    language: "pt-BR",
                     page: 1,
                 }
             })
 
-            console.log(response.data.results);
-
+            // console.log(response.data.results.slice(0, 10));
+            setFilmes(response.data.results.slice(0, 10))
         }
 
         loadFilmes();
@@ -27,8 +29,18 @@ function Home (){
     }, [])
 
     return(
-        <div>
-            <h1>BEM VINDO A HOME</h1>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme) => {
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
