f526e8d2eea4a5933c0984622ba882fa80a1c3ed
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-06-06 14:18:05 -0300

    Creating Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import './header.css'
+import { Link } from 'react-router-dom'
+
+function Header(){
+    return(
+       <header>
+            <Link className="logo" to="/">Prime Flix</Link>
+            <Link className="favoritos" to="/favoritos">Meus Filmes</Link>
+
+        
+       </header> 
+    )
+}
+
+export default Header;
