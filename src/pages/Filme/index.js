726af1b91a74b7f2c0e2966abe7ed74473fd6092
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-06-07 15:13:12 -0300

    Action movie trailer

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -37,6 +37,25 @@ function Filme (){
         }
     }, [navigate, id])
 
+
+    function salvarFilme(){
+        const minhaLista = localStorage.getItem("@primeflix")
+
+        let filmeSalvos = JSON.parse(minhaLista) || [];
+
+        const hasFilme = filmeSalvos.some( (filmeSalvo) => filmeSalvo.id === filme.id)
+
+        if(hasFilme){
+            alert("ESSE FILME JÁ ESTA NA LISTA");
+            return;
+        }
+
+        filmeSalvos.push(filme);
+        localStorage.setItem("@primeflix", JSON.stringify(filmeSalvos))
+        alert("FILME SALVO COM SUCESSO")
+
+    }
+
     if(loading){
         <div className="filme-info">
             <h1>Carregando detalhes...</h1>
@@ -53,7 +72,7 @@ function Filme (){
             <strong>Avaliação: {filme.vote_average} / 10</strong>
 
             <div className="area-buttons">
-                <button>Salvar</button>
+                <button onClick={salvarFilme}>Salvar</button>
                 <button>
                     <a target="blank" rel="external" href={`https://www.youtube.com/results?search_query=${filme.title} Trailer`}>
                         Trailer
