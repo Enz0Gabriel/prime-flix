a14ca05e373a9a067fdbc17b373bf6aaa7e57d7f
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-06-07 14:36:27 -0300

    Film details pt.2

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
+import './filme-info.css';
 import api from '../../services/api';
 
 function Filme (){
@@ -45,9 +45,17 @@ function Filme (){
             <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
 
             <h3>Sinopse</h3>
-            <span>{filme.overview}</span>
-
+            <span>{filme.overview}</span>           
             <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+            <div className="area-buttons">
+                <button>Salvar</button>
+                <button>
+                    <a href="#">
+                        Trailer
+                    </a>
+                </button>
+            </div>
 
         </div>
     )
