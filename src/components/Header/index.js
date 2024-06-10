cae5fc436fd53a8347d5d2dc64e3a4314adbf4cc
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-06-10 14:11:25 -0300

    Finishing

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,6 +5,7 @@ function Header(){
     return(
        <header>
             <Link className="logo" to="/">Prime Flix</Link>
+            
             <Link className="favoritos" to="/favoritos">Meus Filmes</Link>
 
         
